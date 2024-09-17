093530c0e075dd809b1b7f37d56c028b7ae77983
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-09-17 18:49:47 +0100

    add font, change color of background

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import path from "path";
 const app = express();
 
 app.use(express.static(path.resolve(__dirname + "/../public")));
+app.use(express.static(path.resolve(__dirname + "/../assets")));
 app.use("/boards", express.static(path.resolve(__dirname + "/../data/boards")));
 
 app.get("/", (req, res) => {
