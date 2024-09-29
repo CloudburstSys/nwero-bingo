4334ab0c318099acc24f1a7c1b1b184a54cc6457
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-09-29 18:02:52 +0100

    add weighted prompt bucket feature

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,6 @@
+import { describe } from "node:test";
+import BingoCard from "./BingoCard";
+
 /**
  * Shuffles an array in-place.
  * @param array The array to shuffle
@@ -35,4 +38,95 @@ export function onClassChange(element: HTMLElement, callback: (element: HTMLElem
   });
   observer.observe(element, { attributes: true });
   return observer.disconnect;
+}
+
+/**
+ * Save the bingo card state
+ * @param card The card to save
+ * @param expiry When the save expires
+ */
+export function saveState(card: BingoCard, expiry: Date) {
+  window.localStorage.setItem(
+    "card-state",
+    JSON.stringify({
+      expiry,
+      card: card.toJSON(),
+    }),
+  );
+}
+
+/**
+ * Converts an object into an array of objects
+ * @param object The object to convert
+ * @returns An array of the objects keys and values.
+ */
+export function objectToArray(object: any): {key: string, value: any}[] {
+  let output = [];
+  for (let key in object) output.push({ key, value: object[key] });
+  return output;
+}
+
+/**
+ * Loads the prompts in using the weighted prompt bucket system
+ * @param customBuckets Any custom buckets to import
+ * @param weights The weights for the requested buckets
+ * @param spaceCount The amount of spaces available on the board
+ * @returns An array of prompts to put on the board.
+ */
+export async function loadPrompts(customBuckets: { [name: string]: string }, weights: { [name: string]: number }, spaceCount: number): Promise<({ name: string; description: string } | null)[]> {
+  let output: ({ name: string; description: string } | null)[] = [];
+  
+  const defaultBuckets: { [name: string]: string } = {
+    "neuro": "neuro.json",
+    "evil": "evil.json",
+    "vedal": "vedal.json",
+    "chat": "chat.json",
+    "partner": "partner.json"
+  };
+  const buckets: { [name: string]: { name: string; description: string }[] } = {};
+
+  // TODO: This should probably be based on weights....
+  let combinedBuckets = {...defaultBuckets, ...customBuckets};
+  for(let key in weights) {
+    if (combinedBuckets[key] == undefined) {
+      console.error(`Weighs requested bucket "${key}" but that bucket does not exist, Did you forget to import a custom bucket? Prompt loading has failed.`);
+      return [];
+    }
+    let request = await fetch(`/data/boards/${combinedBuckets[key]}`);
+    let prompts: { name: string; description: string }[] = await request.json();
+    shuffleArray(prompts);
+    buckets[key] = prompts;
+  }
+
+  const weightArray = objectToArray(weights);
+  let activeBucket = 0;
+  let count = 0;
+  
+  for (let i = 1; i <= spaceCount; i++) {
+    if (weightArray[activeBucket] == undefined) {
+      console.warn("There are more spaces on the bingo card then there are defined in weights. This will result in an incompleted board. Please fix this in the schedule configuation.");
+      output.push(null);
+      continue;
+    }
+    if (weightArray[activeBucket].value == count) {
+      activeBucket++;
+      count = 0;
+    }
+    if (weightArray[activeBucket] == undefined) {
+      console.warn("There are more spaces on the bingo card then there are defined in weights. This will result in an incompleted board. Please fix this in the schedule configuation.");
+      output.push(null);
+      continue;
+    }
+    if (buckets[weightArray[activeBucket].key][count] == undefined) {
+      console.warn(`There are more prompts requested from the ${weightArray[activeBucket].key} bucket then there are in that bucket. This will result in an incompleted board. Either reduce the amount of prompts requested in schedule configuation or add more prompts to the ${weightArray[activeBucket].key} bucket.`)
+      output.push(null);
+      count++;
+      continue;
+    }
+
+    output.push(buckets[weightArray[activeBucket].key][count]);
+    count++;
+  }
+
+  return output;
 }
